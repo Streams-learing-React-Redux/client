508b9bc7b041fbc553d3895fc00d5c549c3966a1
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2020-11-04 05:00:51 +1100

    router

--- README.md
+++ README.md
@@ -8,6 +8,12 @@ Parts of the projects: React app, API server, RTMP server
 ## 1. Good navigation around all pages in app
 
 - react router
+  ** Dependencies of react-router family **:
+
+1. react-router: is the core library of everything inside of react-router general project.
+2. react-router-dom: browser specific. for handling navigation on dom-based app
+3. react-router-native: Navigation for react-native app (mobile apps)
+4. react-router-redux: bindings between Redux and React Router. a library similler to react-redux library
 
 ## 2. User authentication
 
--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+//dummy component
+const PageOne = () => {
+  return <div>page 1</div>;
+};
+
+const PageTwo = () => {
+  return (
+    <div>
+      page 2<button>click</button>
+    </div>
+  );
+};
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={PageOne} />
+          <Route path="/page2" component={PageTwo} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;

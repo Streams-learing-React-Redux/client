23351986c1002a145b68b39b6380908656c7275a
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2020-11-07 05:43:39 +1100

    Fetching a list of all streams

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,6 +1,19 @@
 import React from "react";
+import { connect } from "react-redux";
+import { fetchStreams } from "../../actions";
 
-const StreamList = () => {
-  return <div className="container-body">StreamList</div>;
-};
-export default StreamList;
+class StreamList extends React.Component {
+  componentDidMount() {
+    this.props.fetchStreams();
+  }
+  render() {
+    return (
+      <div className="container-body">
+        <div>StreamList</div>
+      </div>
+    );
+  }
+}
+export default connect(null, { fetchStreams })(StreamList);
+
+//we need classbased component here because: we nee to cal action creator inside componentDidMount lifecycle method to fetch our list of streams one time

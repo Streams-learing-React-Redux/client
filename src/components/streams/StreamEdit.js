04e04fbc4685afd623d81f7601d38343274d4ab8
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2020-11-08 04:34:50 +1100

    learned that componenet needs to work in isolation while doing any id based action

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -15,3 +15,5 @@ const mapStateToProps = (state, ownProps) => {
 //ownprops is the props that shows up in the component here. its another argument or mapstatetoprorps.
 
 export default connect(mapStateToProps)(StreamEdit);
+
+//big lesson: anytime working with any id based selection, we always have to make sure that any component that is going to be shown on the screen is going to be designed to work by itself or in isolation. it needs to fetch its own data ( )remember how at the begining whenthis edit page renders how the state of the props was undefined? and it shows the stremsa after we click on a edit buton of the list? ..yeah, we are trying to solve that problem, what happpens here that st the begining state props that comes from the store is empty, so thats why it needs to be independent while working alone, like editing or someting.. yay!

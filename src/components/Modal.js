44b1069287b23c37ff676848e0da9bec5d3c094d
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2020-11-09 03:15:24 +1100

    Hidie modal by using history.push(/), and stopped propagation when clicking inside box

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import history from "../history";
+
 const Modal = props => {
   return ReactDOM.createPortal(
-    <div className="modal">
-      <div className="modal-body">
+    <div onClick={() => history.push("/")} className="modal">
+      {/* history.push("/") is taking user to list or home */}
+      <div onClick={e => e.stopPropagation()} className="modal-body">
+        {/* stopPropagation() is stoping to do anything when user clicks inside the modal body */}
         <h3>Delete Stream</h3>
         <div className="modal-body-part">
           <p>Are you sure to delete this stream?</p>

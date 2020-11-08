56d4a8c2790b4da998a0b4696752f452d292d294
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2020-11-09 04:14:01 +1100

    Git propble while fetching stream

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -2,23 +2,28 @@ import React from "react";
 import Modal from "../Modal";
 import history from "../../history";
 
-const StreamDelete = () => {
-  const actions = (
-    <div>
-      <button className="color">Delete</button>
-      <button className="color blue">Cancel</button>
-    </div>
-  );
-  return (
-    <div>
-      StreamDelete
-      <Modal
-        title="Delete Stream"
-        content="Are you sure to delete this stream?"
-        actions={actions}
-        onDismiss={() => history.push("/")}
-      />
-    </div>
-  );
-};
+class StreamDelete extends React.Component {
+  renderActions() {
+    return (
+      <React.Fragment>
+        <button className="color">Delete</button>
+        <button className="color blue">Cancel</button>
+      </React.Fragment>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        StreamDelete
+        <Modal
+          title="Delete Stream"
+          content="Are you sure to delete this stream?"
+          actions={this.renderActions()}
+          onDismiss={() => history.push("/")}
+        />
+      </div>
+    );
+  }
+}
 export default StreamDelete;

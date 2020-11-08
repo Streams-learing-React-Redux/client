a9c4892015383729f4575de704bf9338923a5057
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2020-11-09 05:57:49 +1100

    switch fixes the react-router-dom showing unwanted component

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Router, Route } from "react-router-dom";
+import { Router, Route, Switch } from "react-router-dom";
+//switch for showing only one component through url path
 import StreamCreate from "./streams/StreamCreate";
 import StreamEdit from "./streams/StreamEdit";
 import StreamShow from "./streams/StreamShow";
@@ -15,11 +16,13 @@ const App = () => {
       <Router history={history}>
         <div>
           <Header />
-          <Route path="/" exact component={StreamList} />
-          <Route path="/streams/new" exact component={StreamCreate} />
-          <Route path="/streams/edit/:id" exact component={StreamEdit} />
-          <Route path="/streams/delete/:id" exact component={StreamDelete} />
-          <Route path="/streams/:id" exact component={StreamShow} />
+          <Switch>
+            <Route path="/" exact component={StreamList} />
+            <Route path="/streams/new" exact component={StreamCreate} />
+            <Route path="/streams/edit/:id" exact component={StreamEdit} />
+            <Route path="/streams/delete/:id" exact component={StreamDelete} />
+            <Route path="/streams/:id" exact component={StreamShow} />
+          </Switch>
         </div>
       </Router>
     </div>
@@ -27,3 +30,6 @@ const App = () => {
 };
 
 export default App;
+
+// "/streams/new" and "/streams/:id" problem:
+// /new was showing the streamShow component too. switch allows to show only one route after /

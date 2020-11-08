50b444fba6787bcd93dc6da451f8fc4a108abc6c
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2020-11-09 03:35:16 +1100

    Modal is reusable

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,20 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import history from "../history";
 
 const Modal = props => {
   return ReactDOM.createPortal(
-    <div onClick={() => history.push("/")} className="modal">
-      {/* history.push("/") is taking user to list or home */}
+    <div onClick={props.onDismiss} className="modal">
       <div onClick={e => e.stopPropagation()} className="modal-body">
         {/* stopPropagation() is stoping to do anything when user clicks inside the modal body */}
-        <h3>Delete Stream</h3>
+        <h3>{props.title}</h3>
         <div className="modal-body-part">
-          <p>Are you sure to delete this stream?</p>
-          <div className="modal-body-part-button">
-            <button className="color ">Delete</button>
-            <button className="color blue">Cancel</button>
-          </div>
+          <p>{props.content}</p>
+          <div className="modal-body-part-button">{props.actions}</div>
         </div>
       </div>
     </div>,
--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,11 +1,23 @@
 import React from "react";
 import Modal from "../Modal";
+import history from "../../history";
 
 const StreamDelete = () => {
+  const actions = (
+    <div>
+      <button className="color">Delete</button>
+      <button className="color blue">Cancel</button>
+    </div>
+  );
   return (
     <div>
       StreamDelete
-      <Modal />
+      <Modal
+        title="Delete Stream"
+        content="Are you sure to delete this stream?"
+        actions={actions}
+        onDismiss={() => history.push("/")}
+      />
     </div>
   );
 };

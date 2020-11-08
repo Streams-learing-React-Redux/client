3a54baece9a0157ed0a272c1d908ec1288e5bff3
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2020-11-09 05:25:36 +1100

    delete

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -7,6 +7,7 @@ const Modal = props => {
       <div onClick={e => e.stopPropagation()} className="modal-body">
         {/* stopPropagation() is stoping to do anything when user clicks inside the modal body */}
         <h3>{props.title}</h3>
+
         <div className="modal-body-part">
           <p>{props.content}</p>
           <div className="modal-body-part-button">

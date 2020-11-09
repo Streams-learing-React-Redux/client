70f6155a8cc6126b50be8905d0afde9fa647ac99
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2020-11-10 03:55:49 +1100

    done

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -22,6 +22,11 @@ class StreamShow extends React.Component {
     this.buildPlayer();
   }
 
+  componentWillUnmount() {
+    // console.log("unmounted");
+    this.player.destroy();
+  }
+
   buildPlayer() {
     if (this.player || !this.props.stream) {
       return;
